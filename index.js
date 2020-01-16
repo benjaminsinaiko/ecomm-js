a121defe1c96a207d621c4abcd01a0eb06bd456f
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-16 15:59:56 -0600

    Add middleware to express post request

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('Hi there');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password confirmation" />
+        <input name="passwordConfirmation" placeholder="password" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', data => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
+  res.send('Account created!!!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on :${port}`);
+});
