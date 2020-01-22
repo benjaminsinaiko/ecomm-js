f93c240324e5461e44c1bebad107c3de1632f988
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-22 15:35:11 -0600

    Add multer package for image upload

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 // ################
 // GET - All products
@@ -23,11 +26,23 @@ router.get('/admin/products/new', (req, res) => {
 // ################
 // POST - Add new Product
 // ################
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
+router.post(
+  '/admin/products/new',
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  res.send('submitted');
-});
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }));
+    }
+
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+
+    res.send('submitted');
+  }
+);
 
 module.exports = router;
