9ea8cc330baf9c19a65e45c803f84d878ed3c19e
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-22 09:45:39 -0600

    Add validation for new products post route

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -2,6 +2,15 @@ const { check } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 
 module.exports = {
+  requireTitle: check('title')
+    .trim()
+    .isLength({ min: 5, max: 30 })
+    .withMessage('Must be between 5 and 30 characters'),
+  requirePrice: check('price')
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 })
+    .withMessage('Must be a number greater than 1'),
   requireEmail: check('email')
     .trim()
     .normalizeEmail()
