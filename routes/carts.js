d9cd604e0055a874ad99b5fcd3a850887a11e744
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-24 09:42:37 -0600

    Add create cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
 // ################
 // POST - Add item
 // ################
-router.post('/cart/products', (req, res) => {
-  console.log(req.body.productId);
-
+router.post('/cart/products', async (req, res) => {
+  // Find cart or create
+  let cart;
+  if (!req.session.cartId) {
+    // No cart - create one, add to session
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    // We have a cart, get it from repo
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  console.log(cart);
   res.send('Product added to cart');
 });
 
