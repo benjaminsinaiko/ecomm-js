e3dcbbd1b64fdd97c52d9d2c9aac965a43868b64
benjaminsinaiko <ben.jamin@outlook.com>
2020-01-24 09:49:14 -0600

    Add items to cart

--- routes/carts.js
+++ routes/carts.js
@@ -17,6 +17,16 @@ router.post('/cart/products', async (req, res) => {
     // We have a cart, get it from repo
     cart = await cartsRepo.getOne(req.session.cartId);
   }
+
+  const existingItem = cart.items.find(item => item.id === req.body.productId);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items
+  });
   console.log(cart);
   res.send('Product added to cart');
 });
